0469faba5a375555a9459ee31c06e56baa3f3b5a
Valeria N <v.567insp@gmail.com>
2024-11-03 18:52:58 +0200

    ready mountains web-site

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,34 @@
 import '../scss/style.scss'
+
+// burger
+(function () {
+	const isOpen = document.querySelector(".header__inner-burger")
+	const isClose = document.querySelector(".header__nav-close")
+	const navigation = document.querySelector(".header__nav")
+
+	isOpen.addEventListener('click', () => {
+		navigation.classList.add('active')
+	})
+
+	isClose.addEventListener('click', () => {
+		navigation.classList.remove('active')
+	})
+})();
+
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 40,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+    },
+});
